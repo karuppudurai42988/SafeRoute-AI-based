d1b4534213c5f76d337b244d0f5b15c6e3ba13d1
karuppudurai42988 <karuppudurai429@gmail.com>
2026-07-10 19:09:26 +0530

    Update dartpadweb_plugin_registrant.dart

--- saferoute_mobile/.dart_tool/flutter_build/dartpadweb_plugin_registrant.dart
+++ saferoute_mobile/.dart_tool/flutter_build/dartpadweb_plugin_registrant.dart
@@ -1,11 +1,3 @@
-// Flutter web plugin registrant file.
-//
-// Generated file. Do not edit.
-//
-
-// @dart = 2.13
-// ignore_for_file: type=lint
-
 import 'package:geolocator_web/geolocator_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
